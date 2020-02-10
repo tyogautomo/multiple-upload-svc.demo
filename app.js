05208317eb716032ae6a68ecb48170bdec4be79a
tyogautomo <yoga.utomo27@gmail.com>
2020-02-10 16:29:51 +0700

    get rid node mod

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
 
 const app = express();
 const PORT = 3000;
+const Multer = multer({
+  storage: multer.memoryStorage,
+  limits: 1024 * 1024
+})
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+
+app.post('/upload', Multer.array('image'), (req, res, next) => {
+  res.status(200).json({status: 'success uploading files!'})
+})
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
