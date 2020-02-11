742035d1843526f7f31ad9bc45457cd16d75e48d
tyogautomo <yoga.utomo27@gmail.com>
2020-02-11 17:32:38 +0700

    before go home

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+if (!process.env.NODE_ENV || process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'test') {
+  require('dotenv').config();
+};
+
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
+
+const { sendUploadToGCS } = require('./upload');
 
 const app = express();
 const PORT = 3000;
@@ -15,8 +21,8 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-app.post('/upload', Multer.array('image'), (req, res, next) => {
-  res.status(200).json({status: 'success uploading files!'})
+app.post('/upload', Multer.array('image'), sendUploadToGCS, (req, res, next) => {
+  res.status(200).json({ status: 'success uploading files!' })
 })
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
---
+++ upload.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const { Storage } = require('@google-cloud/storage');
+const CLOUD_BUCKET = process.env.CLOUD_BUCKET
+const storage = new Storage({
+  projectId: process.env.GCLOUD_PROJECT,
+  keyFilename: process.env.KEYFILE_PATH
+});
+const bucket = storage.bucket(CLOUD_BUCKET);
+
+const getPublicUrl = (filename) => {
+  return `https://storage.googleapis.com/${CLOUD_BUCKET}/${filename}`
+}
+
+const sendUploadToGCS = (req, res, next) => {
+
+  if (!req.files) {
+    return next()
+  }
+
+  let promises = [];
+  req.files.forEach((image, index) => {
+    const gcsname = req.headers.decode.username + Date.now() + image.originalname
+    const file = bucket.file(gcsname)
+
+    const promise = new Promise((resolve, reject) => {
+      const stream = file.createWriteStream({
+        metadata: {
+          contentType: image.mimetype
+        }
+      });
+
+      stream.on('finish', async () => {
+        try {
+          req.files[index].cloudStorageObject = gcsname
+          await file.makePublic()
+          req.files[index].cloudStoragePublicUrl = getPublicUrl(gcsname)
+          resolve();
+        } catch (error) {
+          reject(error)
+        }
+      });
+
+      stream.on('error', (err) => {
+        req.files[index].cloudStorageError = err
+        reject(err)
+      });
+
+      stream.end(image.buffer);
+    })
+
+    promises.push(promise)
+  });
+
+  Promise.all(promises)
+    .then(_ => {
+      promises = [];
+      next();
+    })
+    .catch(next);
+}
+
+module.exports = {
+  sendUploadToGCS
+}
